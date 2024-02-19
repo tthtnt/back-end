b8cb8329371dc79fe9c978b792a19a7622133a7a
jack.liu <jack.liu@ihr360.com>
2024-02-19 17:14:46 +0800

    feat:(async)async使用方法

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ router.get('/api', ctx => {
     ctx.body = 'hello Api'
 })
 
+router.get('/async', async (ctx) => {
+    let result = await new Promise((resolve) => {
+        setTimeout(function () {
+            resolve('Hello world 2s later')
+        }, 2000)
+    })
+    ctx.body = result
+})
+
 // 1.request method responed
 // 2. api url function router
 // 3. ctx async

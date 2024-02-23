d4fba10b27daba241b4b058fdaf0ba8801b68a49
jack.liu <jack.liu@ihr360.com>
2024-02-23 16:09:04 +0800

    feat:使用koa-compose 集成中间件

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import path from 'path'
 import helmet from 'koa-helmet'
 import statics from 'koa-static'
 import router from './routes/routes'
+import jsonutil from 'koa-json'
+import cors from '@koa/cors'
+import compose from 'koa-compose'
+import koaBody from 'koa-body'
 
 const app = new Koa()
-// 处理header
-app.use(helmet())
-// 处理body
-app.use(KoaBody())
-// 处理静态文件
-app.use(statics(path.join(__dirname, '../public')))
+
+const middleware = compose([
+    koaBody(),
+    statics(path.join(__dirname, '../public')),
+    cors(),
+    jsonutil({pretty: false, param: 'pretty'}),
+    helmet()
+])
+
+/**
+ * 使用koa-compose 集成中间件
+ */
+app.use(middleware)
 app.use(router())
 
 console.log("listen success 3001")
